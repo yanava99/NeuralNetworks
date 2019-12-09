f571b5bd72ae2397313ba937d35f447de2241189
yanava99 <47676411+yanava99@users.noreply.github.com>
2019-12-09 10:17:24 +0300

    Update NeuralNetworksLibrary.py

--- NeuralNetworksLibrary.py
+++ NeuralNetworksLibrary.py
@@ -6,10 +6,15 @@ import copy
 # and call derivative receiving result.
 # It's going to be used when specifying activation functions for the layers.
 class ActivationFunction:
-    def __init__(self, function, derivative, derivativeFromAnswer):
+    def __init__(self, function, derivative, derivativeFromAnswer, multivariate=False):
         self.func = function
-        self.derivative = np.vectorize(derivative)
-        self.derivativeFromAnswer = np.vectorize(derivativeFromAnswer)
+        self.multivariate = multivariate
+        if multivariate:
+            self.derivativeFromAnswer = derivativeFromAnswer
+            self.derivative = derivative
+        else:
+            self.derivativeFromAnswer = np.vectorize(derivativeFromAnswer)
+            self.derivative = np.vectorize(derivative)
 
     def __call__(self, x):
         return self.func(x)
@@ -36,18 +41,15 @@ class ErrorFunction:
         return self.derivative(x, y)
 
 
-# Class Layer has information about number of neurons, activation function, dropout probability,
-# current weights and bias.
+# Class Layer has information about number of neurons, activation function, current weights and bias.
 class Layer:
-    def __init__(self, n, function, dropout=0):
+    def __init__(self, n, function):
         self.n = n
         self.function = function
-        self.dropout = dropout
         self.weights = np.array([[]])
         self.bias = np.array([])
 
     def setWeights(self, array):
-        # specify dimensions
         self.weights = array.copy()
 
     def setBias(self, array):
@@ -71,7 +73,7 @@ class Layer:
         self.bias -= step * grad
 
 
-# Class Model sets structure of the future Model.
+# Class Model sets structure of the future neural network.
 # It is changeable: you can add new layers.
 class Model:
     def __init__(self, dimInput, dimOutput):
@@ -86,11 +88,12 @@ class Model:
 # Class Network is not changeable, its entity is created on the base of a model.
 class Network:
     def __init__(self, model, errorFunc):  # add a default errorFunc
-        # add a check if number of neurons on the top layer equals dimOutput
-        # OR if they are not equal add a layer (with(out) activation function? weights?)
         self.dimX = model.dimX
         self.dimY = model.dimY
         self.layers = copy.deepcopy(model.layers)
+        if self.layers[-1].n != self.dimY:
+            newLayer = Layer(self.dimY, identity)
+            self.layers.append(newLayer)
         for i in range(len(self.layers)):
             if self.layers[i].weights.size == 0:
                 if i == 0:
@@ -101,38 +104,25 @@ class Network:
                 self.layers[i].setDefaultBias()
         self.errorFunc = errorFunc
 
-    def fit(self, x, y, step=0.5):
+    # Method for training the network.
+    def fit(self, x, y, step=0.1):
         tempX = [x]  # saving outputs of each neuron
         for layer in self.layers:
             x = layer.goThrough(x)
             tempX.append(x)
         buffer = self.errorFunc.callDerivative(x, y)  # accumulation of derivatives
         for i in reversed(range(len(self.layers))):
-            buffer *= self.layers[i].function.derivativeFromAnswer(tempX[i + 1])
+            if self.layers[i].function.multivariate:
+                buffer = buffer @ self.layers[i].function.derivativeFromAnswer(tempX[i + 1])
+            else:
+                buffer *= self.layers[i].function.derivativeFromAnswer(tempX[i + 1])
             self.layers[i].updateBias(buffer, step)
             gradWeights = buffer.reshape(buffer.shape[0], 1) @ tempX[i].reshape(1, tempX[i].shape[0])
             buffer = buffer @ self.layers[i].weights
             self.layers[i].updateWeights(gradWeights, step)
         return self.errorFunc(x, y)
 
-    def fitForCrossEntropy(self, x, y, step=0.5):
-        tempX = [x]  # saving outputs of each neuron
-        for layer in self.layers:
-            x = layer.goThrough(x)
-            tempX.append(x)
-        buffer = - y * (1 - tempX[-1])
-        self.layers[-1].updateBias(buffer, step)
-        gradWeights = buffer.reshape(buffer.shape[0], 1) @ tempX[-2].reshape(1, tempX[-2].shape[0])
-        buffer = buffer @ self.layers[-1].weights
-        self.layers[-1].updateWeights(gradWeights, step)
-        for i in reversed(range(len(self.layers) - 1)):
-            buffer *= self.layers[i].function.derivativeFromAnswer(tempX[i + 1])
-            self.layers[i].updateBias(buffer, step)
-            gradWeights = buffer.reshape(buffer.shape[0], 1) @ tempX[i].reshape(1, tempX[i].shape[0])
-            buffer = buffer @ self.layers[i].weights
-            self.layers[i].updateWeights(gradWeights, step)
-        # return self.errorFunc(x, y)
-
+    # Setting weights and bias to default values.
     def setDefaultParameters(self):
         for i in range(len(self.layers)):
             if i == 0:
@@ -141,27 +131,30 @@ class Network:
                 self.layers[i].setDefaultWeights(self.layers[i - 1].n)
             self.layers[i].setDefaultBias()
 
+    # Method for predicting.
     def evaluate(self, x):
         for layer in self.layers:
             x = layer.goThrough(x)
+        if self.errorFunc == softmaxAndCrossEntropyBundleError:
+            x = softmax(x)
         return x
 
 
-# Implementations of commonly used activation functions.
+# Implementations of commonly used activation functions and their derivatives.
 
-def logisticFunc(x):
+def logisticFunction(x):
     return 1 / (1 + np.exp(-x))
 
 
 def logisticDerivative(x):
-    return logisticFunc(x) * (1 - logisticFunc(x))
+    return logisticFunction(x) * (1 - logisticFunction(x))
 
 
 def logisticDerivativeFromAnswer(ans):
     return ans * (1 - ans)
 
 
-def reLUFunc(x):
+def reLUFunction(x):
     if x < 0:
         return 0.
     else:
@@ -182,7 +175,7 @@ def reLUDerivativeFromAnswer(ans):
         return 0.
 
 
-def softmaxFunc(x):
+def softmaxFunction(x):
     div = 0
     maximum = np.amax(x)
     for xi in x:
@@ -192,19 +185,30 @@ def softmaxFunc(x):
 
 
 def softmaxDerivative(x):
-    # derivative is computed only with respect to the argument coordinate with the same index
-    return softmaxFunc(x) * (1 - softmaxFunc(x))
+    return softmaxDerivativeFromAnswer(softmaxFunction(x))
 
 
 def softmaxDerivativeFromAnswer(ans):
-    # derivative is computed only with respect to the argument coordinate with the same index
-    return ans * (1 - ans)
+    der = np.asarray([[- ans[i] * ans[j] if i != j else ans[i] * (1 - ans[i]) for j in range(ans.shape[0])]
+                      for i in range(ans.shape[0])])
+    return der
 
 
-# Implementations of commonly used error functions.
+def identityFunction(x):
+    return x
+
+
+def identityDerivative(x):
+    return 1
+
+
+def identityDerivativeFromAnswer(ans):
+    return 1
+
+
+# Implementations of commonly used error functions and their derivatives.
 
 def squaredErrorFunction(x, y):
-    # add a check if x.size == y.size
     result = 0.
     for i in range(x.size):
         result += (y[i] - x[i])**2
@@ -213,13 +217,10 @@ def squaredErrorFunction(x, y):
 
 
 def squaredErrorDerivative(x, y):
-    # works for numpy.array
-    # each element of a result contains derivative on this component
     return x - y
 
 
 def crossEntropyErrorFunction(x, y):
-    # add a check if x.size == y.size
     result = 0.
     for i in range(x.size):
         result += y[i] * np.log(x[i])
@@ -231,11 +232,22 @@ def crossEntropyErrorDerivative(x, y):
     return -y / x
 
 
+def softmaxAndCrossEntropyBundleErrorFunction(x, y):
+    return crossEntropyErrorFunction(softmaxFunction(x), y)
+
+
+def softmaxAndCrossEntropyBundleErrorDerivative(x, y):
+    return softmax(x) - y
+
+
 # Most commonly used activation and error functions.
 
-logistic = ActivationFunction(logisticFunc, logisticDerivative, logisticDerivativeFromAnswer)
-reLU = ActivationFunction(reLUFunc, reLUDerivative, reLUDerivativeFromAnswer)
-softmax = ActivationFunction(softmaxFunc, softmaxDerivative, softmaxDerivativeFromAnswer)
+logistic = ActivationFunction(logisticFunction, logisticDerivative, logisticDerivativeFromAnswer)
+reLU = ActivationFunction(reLUFunction, reLUDerivative, reLUDerivativeFromAnswer)
+softmax = ActivationFunction(softmaxFunction, softmaxDerivative, softmaxDerivativeFromAnswer, True)
+identity = ActivationFunction(identityFunction, identityDerivative, identityDerivativeFromAnswer)
 
 squaredError = ErrorFunction(squaredErrorFunction, squaredErrorDerivative)
 crossEntropyError = ErrorFunction(crossEntropyErrorFunction, crossEntropyErrorDerivative)
+softmaxAndCrossEntropyBundleError = ErrorFunction(softmaxAndCrossEntropyBundleErrorFunction,
+                                                  softmaxAndCrossEntropyBundleErrorDerivative)
